7ca0ef688798f85bdafd135aaf50f1ccfcb33abe
tarn0 <33187177+tarn0@users.noreply.github.com>
2022-11-10 21:08:39 +0900

    Update 6axis_test.py

--- moduletest/lsm303dlh/6axis_test.py
+++ moduletest/lsm303dlh/6axis_test.py
@@ -2,13 +2,23 @@
 import time
 import board
 import adafruit_lsm303dlh_mag
+import csv
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 sensor = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
 
-while True:
+t = 0
+duration = 0.5
+maglist = []
+while t <= 60:
     mag_x, mag_y, mag_z = sensor.magnetic
-
+    maglist.append([mag_x,mag_y,mag_z])
     print('Magnetometer (gauss): ({0:10.3f}, {1:10.3f}, {2:10.3f})'.format(mag_x, mag_y, mag_z))
     print('')
-    time.sleep(1.0)
+    
+    t += duration
+    time.sleep(duration)
+    
+with open('lsm303.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(maglist)
