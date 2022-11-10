ad49fd7846ea41203f61b9ec44f4191567618efb
shota182 <104834704+shota182@users.noreply.github.com>
2022-11-10 21:32:06 +0900

    Update nicrom.py

--- moduletest/nicrom/nicrom.py
+++ moduletest/nicrom/nicrom.py
@@ -5,9 +5,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(22,  GPIO.OUT)
 
 GPIO.output(22, True)
-#ここの数字は実験次第
 print("start")
-time.sleep(2)
+#ここの数字は実験次第
+time.sleep(3)
 GPIO.output(22, False)
 GPIO.cleanup()
 print("end")
