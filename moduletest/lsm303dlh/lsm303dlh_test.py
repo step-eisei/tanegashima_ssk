81168de2d3b50debecf762df2d125b9f0aeba0b0
matsuiiiiiii <117170622+matsuiiiiiii@users.noreply.github.com>
2022-11-15 20:55:25 +0900

    Update lsm303dlh_test.py

--- moduletest/lsm303dlh/lsm303dlh_test.py
+++ moduletest/lsm303dlh/lsm303dlh_test.py
@@ -5,6 +5,13 @@ import adafruit_lsm303dlh_mag
 import csv
 import sys
 
+def percentpick(listdata):
+    n = int(len(listdata) *p/100)
+    listdata = sorted(listdata) # 昇順
+    min = listdata[n-1]
+    max = listdata[len(listdata)-n]
+    return max, min
+
 i2c = board.I2C()  # uses board.SCL and board.SDA
 sensor = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
 
@@ -17,6 +24,10 @@ while t <= 5:
     time.sleep(duration)
 i = 1
 try:
+    with open('mag.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['magX_max', 'magX_min', 'magY_max', 'magY_min', 'magZ_max', 'magZ_min'])
+        f.close()
     while True:
   
         t = 0
@@ -28,7 +39,18 @@ try:
         
             t += duration
             time.sleep(duration)
+    
 
+        # 最大値，最小値の算出
+        p = 5 # 上位何%をpickするか
+        Xmax, Xmin = percentpick(mag_x)
+        Ymax, Ymin = percentpick(mag_y)
+        Zmax, Zmin = percentpick(mag_z)
+       
+        with open('mag.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([Xmax, Xmin, Ymax, Ymin, Zmax, Zmin])
+            f.close()
         with open(f'lsm303_No{i}.csv', 'w') as f:
             writer = csv.writer(f)
             writer.writerows(maglist)
