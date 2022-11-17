21568c9535cbf6e05aa06b6ba36801089ed6aeb9
shota182 <104834704+shota182@users.noreply.github.com>
2022-11-17 20:56:11 +0900

    Update drv8835_test.py

--- moduletest/drv8835/drv8835_test.py
+++ moduletest/drv8835/drv8835_test.py
@@ -1,22 +1,124 @@
-# 参考サイト：https://www.denshi.club/parts/2020/11/gpiozero8import-motor-2-tb6612.html
+import RPi.GPIO as GPIO
+import time
+
+PIN_AIN1 = 24
+PIN_AIN2 = 23
+PIN_PWMA = 12
+PIN_BIN1 = 16
+PIN_BIN2 = 26
+PIN_PWMB = 13
+DUTY_A = 10
+DUTY_B = 10
+
+ROT_DUR = 10 # [s]
+
+GPIO.setmode(GPIO.BCM)
+
+# 左モータ
+GPIO.setup(PIN_AIN1, GPIO.OUT)
+GPIO.setup(PIN_AIN2, GPIO.OUT)
+
+# 左モータPWM
+GPIO.setup(PIN_PWMA, GPIO.OUT)
+pwm_left = GPIO.PWM(PIN_PWMA, 300)
+pwm_left.start(10)
+pwm_left.ChangeDutyCycle(DUTY_A)
+
+# 右モータ
+GPIO.setup(PIN_BIN1, GPIO.OUT)
+GPIO.setup(PIN_BIN2, GPIO.OUT)
+
+# 右モータPWM
+GPIO.setup(PIN_PWMB, GPIO.OUT)
+pwm_right = GPIO.PWM(PIN_PWMB, 300)
+pwm_right.start(10)
+pwm_right.ChangeDutyCycle(DUTY_B)
+
+# sleep
+time.sleep(2)
+
+# ----------------------------------------
+
+# # 前進
+# GPIO.output(PIN_AIN1, GPIO.LOW)
+# GPIO.output(PIN_AIN2, GPIO.HIGH)
+# GPIO.output(PIN_BIN1, GPIO.HIGH)
+# GPIO.output(PIN_BIN2, GPIO.LOW)
+# time.sleep(3)
+
+# # 後進
+# GPIO.output(PIN_AIN1, GPIO.HIGH)
+# GPIO.output(PIN_AIN2, GPIO.LOW)
+# GPIO.output(PIN_BIN1, GPIO.LOW)
+# GPIO.output(PIN_BIN2, GPIO.HIGH)
+# time.sleep(3)
+
+# 右旋回
+GPIO.output(PIN_AIN1, GPIO.HIGH)
+GPIO.output(PIN_AIN2, GPIO.LOW)
+GPIO.output(PIN_BIN1, GPIO.HIGH)
+GPIO.output(PIN_BIN2, GPIO.LOW)
+time.sleep(ROT_DUR)
+
+# 右モータ停止 左モータ停止
+GPIO.output(PIN_AIN1, GPIO.LOW)
+GPIO.output(PIN_AIN2, GPIO.LOW)
+GPIO.output(PIN_BIN1, GPIO.LOW)
+GPIO.output(PIN_BIN2, GPIO.LOW)
+time.sleep(3)
+
+# 左旋回
+GPIO.output(PIN_AIN1, GPIO.LOW)
+GPIO.output(PIN_AIN2, GPIO.HIGH)
+GPIO.output(PIN_BIN1, GPIO.LOW)
+GPIO.output(PIN_BIN2, GPIO.HIGH)
+time.sleep(ROT_DUR)
 
 
+# ----------------------------------------
 
-from gpiozero import Motor
-from time import sleep
+# # 右モータ後進
+# GPIO.output(PIN_AIN1, GPIO.HIGH)
+# GPIO.output(PIN_AIN2, GPIO.LOW)
+# time.sleep(3)
 
-print("forward")
-motor1 = Motor(23, 24)
-motor2 = Motor(16, 26)
-motor1.forward(0.2)
-motor2.forward(0.2)
-sleep(2)
+# # 右モータブレーキ？
+# GPIO.output(PIN_AIN1, GPIO.HIGH)
+# GPIO.output(PIN_AIN2, GPIO.HIGH)
+# time.sleep(1)
 
-print("backward")
-motor1.backward(0.5)
-motor2.backward(0.5)
-sleep(3)
+# # 右モータ前進
+# GPIO.output(PIN_AIN1, GPIO.LOW)
+# GPIO.output(PIN_AIN2, GPIO.HIGH)
+# time.sleep(3)
 
-print("stop")
-motor1.stop()
-motor2.stop()
+# # 右モータ停止？
+# GPIO.output(PIN_AIN1, GPIO.LOW)
+# GPIO.output(PIN_AIN2, GPIO.LOW)
+# time.sleep(3)
+
+# #----------------------------------------
+# # 左モータ前進
+# GPIO.output(PIN_BIN1, GPIO.HIGH)
+# GPIO.output(PIN_BIN2, GPIO.LOW)
+# time.sleep(3)
+
+# # 左モータブレーキ？
+# GPIO.output(PIN_BIN1, GPIO.HIGH)
+# GPIO.output(PIN_BIN2, GPIO.HIGH)
+# time.sleep(1)
+
+# # 左モータ後進
+# GPIO.output(PIN_BIN1, GPIO.LOW)
+# GPIO.output(PIN_BIN2, GPIO.HIGH)
+# time.sleep(3)
+
+# # 左モータ停止？
+# GPIO.output(PIN_BIN1, GPIO.LOW)
+# GPIO.output(PIN_BIN2, GPIO.LOW)
+# time.sleep(3)
+
+#----------------------------------------
+pwm_left.stop()
+pwm_right.stop()
+GPIO.cleanup()
