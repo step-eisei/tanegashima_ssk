9837a16c7827e1629315fb4c689fe48e997f738c
shota182 <104834704+shota182@users.noreply.github.com>
2022-12-06 21:10:32 +0900

    Update hcsr04_test02.py

--- moduletest/hcsr04/hcsr04_test02.py
+++ moduletest/hcsr04/hcsr04_test02.py
@@ -1,6 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import time#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import time
+import RPi.GPIO as GPIO
+
+def reading(sensor):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    if sensor == 0:
+        GPIO.setup(TRIG,GPIO.OUT)
+        GPIO.setup(ECHO,GPIO.IN)
+        GPIO.output(TRIG, GPIO.LOW)
+        time.sleep(0.3)
+
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        while GPIO.input(ECHO) == 0:
+            signaloff = time.time()
+        while GPIO.input(ECHO) == 1:
+            signalon = time.time()
+            waiting_time = time.time()
+            if(time.time()-waiting_time > 2):
+                print("reception error.")
+                break
+
+        timepassed = signalon - signaloff
+        distance = timepassed * 17000
+        return distance
+        GPIO.cleanup()
+    else:
+        print("Incorrect usonic() function varible.")
+
+def main():
+    while(True):
+        try:
+            print(reading(0))
+            time.sleep(1)
+        except KeyboardInterrupt:
+            GPIO.output(TRIG, False)
+            GPIO.cleanup()
+            break
+    print("program fin.")
+
+if __name__ == "__main__":
+    TRIG = 17
+    ECHO = 27
+    main()
+    pass
 import RPi.GPIO as GPIO
 
 def reading():
