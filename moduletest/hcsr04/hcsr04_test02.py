37857e33869ad309916e7959fd40764324cfea32
shota182 <104834704+shota182@users.noreply.github.com>
2022-12-06 21:25:40 +0900

    Update hcsr04_test02.py

--- moduletest/hcsr04/hcsr04_test02.py
+++ moduletest/hcsr04/hcsr04_test02.py
@@ -1,54 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import time
 import RPi.GPIO as GPIO
 
 def reading(sensor):
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-
-    if sensor == 0:
-        GPIO.setup(TRIG,GPIO.OUT)
-        GPIO.setup(ECHO,GPIO.IN)
-        GPIO.output(TRIG, GPIO.LOW)
-        time.sleep(0.3)
-
-        GPIO.output(TRIG, True)
-        time.sleep(0.00001)
+    try:
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        if sensor == 0:
+            GPIO.setup(TRIG,GPIO.OUT)
+            GPIO.setup(ECHO,GPIO.IN)
+            GPIO.output(TRIG, GPIO.LOW)
+            time.sleep(0.3)
+            GPIO.output(TRIG, True)
+            time.sleep(0.00001)
+            GPIO.output(TRIG, False)
+            while GPIO.input(ECHO) == 0:
+                signaloff = time.time()
+            while GPIO.input(ECHO) == 1:
+                signalon = time.time()
+            timepassed = signalon - signaloff
+            distance = timepassed * 17000
+            return distance
+            GPIO.cleanup()
+        else:
+            print("Incorrect usonic() function varible.")
+    except KeyboardInterrupt:
         GPIO.output(TRIG, False)
-
-        while GPIO.input(ECHO) == 0:
-            signaloff = time.time()
-        while GPIO.input(ECHO) == 1:
-            signalon = time.time()
-            waiting_time = time.time()
-            if(time.time()-waiting_time > 2):
-                print("reception error.")
-                break
-
-        timepassed = signalon - signaloff
-        distance = timepassed * 17000
-        return distance
         GPIO.cleanup()
-    else:
-        print("Incorrect usonic() function varible.")
+        return 0
 
 def main():
     while(True):
-        try:
-            print(reading(0))
-            time.sleep(1)
-        except KeyboardInterrupt:
-            GPIO.output(TRIG, False)
-            GPIO.cleanup()
+        length = reading(0)
+        if(length != 0): print(length)
+        else:
+            print("program fin.")
             break
-    print("program fin.")
+    pass
 
 if __name__ == "__main__":
     TRIG = 17
     ECHO = 27
     main()
     pass
-import RPi.GPIO as GPIO
