b58746ced2ec671dd2732ec6aeb6dd24a05f38f8
shota182 <104834704+shota182@users.noreply.github.com>
2022-12-06 21:16:11 +0900

    Update hcsr04_test02.py

--- moduletest/hcsr04/hcsr04_test02.py
+++ moduletest/hcsr04/hcsr04_test02.py
@@ -52,35 +52,3 @@ if __name__ == "__main__":
     main()
     pass
 import RPi.GPIO as GPIO
-
-def reading():
-    GPIO.setwarnings(False)
-     
-    GPIO.setmode(GPIO.BCM)
-    TRIG = 17
-    ECHO = 27
-    try:
-        while True:
-            try:
-                GPIO.setup(TRIG,GPIO.OUT)
-                GPIO.setup(ECHO,GPIO.IN)
-                GPIO.output(TRIG, GPIO.LOW)
-                time.sleep(0.3)
-                
-                GPIO.output(TRIG, True)
-                time.sleep(0.00001)
-                GPIO.output(TRIG, False)
-        
-                while GPIO.input(ECHO) == 0: signaloff = time.time()
-                
-                while GPIO.input(ECHO) == 1: signalon = time.time()
-        
-                timepassed = signalon - signaloff
-                distance = timepassed * 17000
-                print(distance)
-            except: print(0)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-         
-if __name__ == "__main__":
-    reading()
