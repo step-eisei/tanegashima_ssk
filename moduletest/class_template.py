0c674b478aae1c7f7d13078a8d14580b1b05f2cd
shota182 <104834704+shota182@users.noreply.github.com>
2023-01-10 20:35:55 +0900

    クラスのテンプレ

---
+++ moduletest/class_template.py
@@ -0,0 +1,19 @@
+# クラスのテンプレート
+
+class Template:
+    
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        pass
+    
+    def function1(self):
+        pass
+
+def main():
+    template = Template(1, 2)
+    print(template.a)
+
+if __name__ == "__main__":
+    main()
+    pass
