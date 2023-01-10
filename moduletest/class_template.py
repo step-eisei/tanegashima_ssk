c810a9fdd423e4b7b5421e502fe96cc651150f85
shota182 <104834704+shota182@users.noreply.github.com>
2023-01-10 20:40:06 +0900

    Update class_template.py

--- moduletest/class_template.py
+++ moduletest/class_template.py
@@ -8,11 +8,13 @@ class Template:
         pass
     
     def function1(self):
+        print(self.b)
         pass
 
 def main():
     template = Template(1, 2)
     print(template.a)
+    template.function1()
 
 if __name__ == "__main__":
     main()
