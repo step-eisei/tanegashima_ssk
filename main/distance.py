b39a2f57fa03da5a98e632997f624d106d31730e
matsuiiiiiii <117170622+matsuiiiiiii@users.noreply.github.com>
2023-01-10 22:28:07 +0900

    Create distance.py

---
+++ main/distance.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import RPi.GPIO as GPIO
+
+
+class Distance:
+    
+    def __init__(self,TRIG,ECHO):
+            self.TRIG = TRIG
+            self.ECHO = ECHO
+            pass
+
+    def reading(self,sensor = 0):
+        try:
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BCM)
+            if sensor == 0:
+                GPIO.setup(self.TRIG,GPIO.OUT)
+                GPIO.setup(self.ECHO,GPIO.IN)
+                GPIO.output(self.TRIG, GPIO.LOW)
+                time.sleep(0.3)
+                GPIO.output(self.TRIG, True)
+                time.sleep(0.00001)
+                GPIO.output(self.TRIG, False)
+                while GPIO.input(self.ECHO) == 0:
+                    signaloff = time.time()
+                while GPIO.input(self.ECHO) == 1:
+                    signalon = time.time()
+                timepassed = signalon - signaloff
+                distance = timepassed * 17000
+                return distance
+                GPIO.cleanup()
+            else:
+                print("Incorrect usonic() function varible.")
+        except KeyboardInterrupt:
+            GPIO.output(self.TRIG, False)
+            GPIO.cleanup()
+
+
+def main():
+    while(True):
+        distance = Distance(17,27)
+        print(distance.reading())
+        time.sleep(1)
+    pass
+
+if __name__ == "__main__":
+    main()
+    pass
