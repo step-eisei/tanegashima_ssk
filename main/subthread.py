4e229cc8d8a32df95bb69dac0efd5719666cee0b
shota182 <104834704+shota182@users.noreply.github.com>
2023-01-17 22:21:32 +0900

    Create subthread.py

---
+++ main/subthread.py
@@ -0,0 +1,19 @@
+# サブスレッド処理
+
+class Subthread:
+    
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        pass
+    
+    def function1(self):
+        pass
+
+def main():
+    template = Template(1, 2)
+    print(template.a)
+
+if __name__ == "__main__":
+    main()
+    pass
