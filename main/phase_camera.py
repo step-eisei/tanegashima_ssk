8c914f3979ca148b9a0f83bf677b80998c03e819
shota182 <104834704+shota182@users.noreply.github.com>
2023-01-17 19:38:55 +0900

    Create phase_camera.py

---
+++ main/phase_camera.py
@@ -0,0 +1,15 @@
+# カメラフェーズ
+
+import class_yolo
+import class_geomag
+import class_gps
+
+class Phase_camera:
+  def __init__(self):
+    pass
+  
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
