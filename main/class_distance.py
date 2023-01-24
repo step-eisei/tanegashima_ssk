ff84bfe07a91397ebf73b7087ddbeb77d8396b60
shota182 <104834704+shota182@users.noreply.github.com>
2023-01-24 22:56:02 +0900

    Update class_distance.py

--- main/class_distance.py
+++ main/class_distance.py
@@ -20,22 +20,29 @@ class Distance:
             
             if sensor == 0:
                 #データ取得
-                for j in range(5):
-                    GPIO.setup(self.TRIG,GPIO.OUT)
-                    GPIO.setup(self.ECHO,GPIO.IN)
-                    GPIO.output(self.TRIG, GPIO.LOW)
-                    time.sleep(0.3)
-                    GPIO.output(self.TRIG, True)
-                    time.sleep(0.00001)
-                    GPIO.output(self.TRIG, False)
-                    while GPIO.input(self.ECHO) == 0:
-                        signaloff = time.time()
-                    while GPIO.input(self.ECHO) == 1:
-                        signalon = time.time()
-                    timepassed = signalon - signaloff
-                    distance = timepassed * 17000
-                    distance_data.append(distance)
-                    print(f"data no.{j}")
+                cycle=1
+                while(cycle<6):
+                    try:
+                        GPIO.setup(self.TRIG,GPIO.OUT)
+                        GPIO.setup(self.ECHO,GPIO.IN)
+                        GPIO.output(self.TRIG, GPIO.LOW)
+                        time.sleep(0.3)
+                        GPIO.output(self.TRIG, True)
+                        time.sleep(0.00001)
+                        GPIO.output(self.TRIG, False)
+                        while GPIO.input(self.ECHO) == 0:
+                            signaloff = time.time()
+                        while GPIO.input(self.ECHO) == 1:
+                            signalon = time.time()
+                        timepassed = signalon - signaloff
+                        distance = timepassed * 17000
+                        distance_data.append(distance)
+                        print(f"data no.{cycle}")
+                        cycle+=1
+                    except:
+                        GPIO.output(self.TRIG, False)
+                        print("overflow")
+                        
 
                 #外れ値を除外   
                 ave = np.mean(distance_data)
