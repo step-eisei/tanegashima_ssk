70ab3bcbc022be60087c1697104e0feec8825b23
unknown <taroarima2014@gmail.com>
2023-01-24 22:01:51 +0900

    modified:   main/class_distance.py

--- main/class_distance.py
+++ main/class_distance.py
@@ -17,10 +17,9 @@ class Distance:
            
             GPIO.setwarnings(False)
             GPIO.setmode(GPIO.BCM)
-            print("try")
+            
             if sensor == 0:
                 #データ取得
-                print("if")
                 for j in range(5):
                     GPIO.setup(self.TRIG,GPIO.OUT)
                     GPIO.setup(self.ECHO,GPIO.IN)
@@ -29,13 +28,10 @@ class Distance:
                     GPIO.output(self.TRIG, True)
                     time.sleep(0.00001)
                     GPIO.output(self.TRIG, False)
-                    print("for")
                     while GPIO.input(self.ECHO) == 0:
                         signaloff = time.time()
-                    print("off")
                     while GPIO.input(self.ECHO) == 1:
                         signalon = time.time()
-                    print("on")
                     timepassed = signalon - signaloff
                     distance = timepassed * 17000
                     distance_data.append(distance)
@@ -44,12 +40,10 @@ class Distance:
                 ave = np.mean(distance_data)
                 max = 0
                 num = 0
-                print("before")
                 for i in range(len(distance_data)):
                     if((ave-distance_data[i])**2 > max):
                         num = i
                         max = (ave-distance_data[i])**2
-                print("after")
                 distance_data = np.delete(distance_data, num)
                 print(distance_data)
                 self.distance = np.mean(distance_data)
@@ -63,12 +57,9 @@ class Distance:
 
 
 def main():
-    print("main")
     while(True):
         distance = Distance(17,27)
-        print("1")
         distance.reading()
-        print("2")
         print(distance.distance)
         time.sleep(1)
     pass
