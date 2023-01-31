909e7d94f83728d7557e1d3bf7988ea9a04b2ea5
HayatoIshii <90325360+HayatoIshii@users.noreply.github.com>
2023-01-31 22:18:35 +0900

    Update phase_camera.py

--- main/phase_camera.py
+++ main/phase_camera.py
@@ -30,60 +30,60 @@ class Phase_camera:
     
     def run(self):
          i = 0
-    bool_mag = True
-    bool_takephoto = True
-    bool_redcone = True
-    while(bool_mag):
-        # get geomag
-        geomag.get()
-        while(bool_takephoto):
-            # take a photo
-            --
-            # image-processing
-            --
-            
-            while(bool_redcone):
-                if(0):# red cone in photo
-                    # rotate to red cone
-                    --
-                    # take a photo
-                    --
-                    # image-processing
-                    --
-                    if(0):# red cone in the center of image
-                        if(0):# distance of red cone is 1m
-                            # goto phase_distance
-                            return 0
-                            pass
+        bool_mag = True
+        bool_takephoto = True
+        bool_redcone = True
+        while(bool_mag):
+            # get geomag
+            geomag.get()
+            while(bool_takephoto):
+                # take a photo
+                --
+                # image-processing
+                --
+
+                while(bool_redcone):
+                    if(0):# red cone in photo
+                        # rotate to red cone
+                        --
+                        # take a photo
+                        --
+                        # image-processing
+                        --
+                        if(0):# red cone in the center of image
+                            if(0):# distance of red cone is 1m
+                                # goto phase_distance
+                                return 0
+                                pass
+                            else:
+                                # forward n sec.
+                                --
+                                bool_takephoto = False
+                                bool_redcone = False
+                        else: pass
+                    else:
+                        if(i<12):
+                            #rotate 30 deg.
+                            deployment.rotate(30)
+                            i += 1
+                            bool_redcone = False
                         else:
                             # forward n sec.
-                            --
-                            bool_takephoto = False
-                            bool_redcone = False
-                    else: pass
-                else:
-                    if(i<12):
-                        #rotate 30 deg.
-                        deployment.rotate(30)
-                        i += 1
-                        bool_redcone = False
-                    else:
-                        # forward n sec.
-                        forward_time = n
-                        # set duty
-                        duty = 60
-                        print("forward start for" + str(forward_time)+ "seconds")
-                        for i in range(10):
-                            duty_new = int(duty/10*(i+1))
-                            motor.changeduty(duty_new, duty_new)
-                            time.sleep(forward_time/10)
-                    
-                        # get gps
-                        if(0):# distance of red cone is over3m
-                            # goto gps
-                            return -1
-                            pass
-                        else: bool_redcone = False
+                            forward_time = n
+                            # set duty
+                            duty = 60
+                            print("forward start for" + str(forward_time)+ "seconds")
+                            for i in range(10):
+                                duty_new = int(duty/10*(i+1))
+                                motor.changeduty(duty_new, duty_new)
+                                time.sleep(forward_time/10)
+
+                            # get gps
+                            if(0):# distance of red cone is over3m
+                                # goto gps
+                                return -1
+                                pass
+                            else: bool_redcone = False
         
     
 def main():
