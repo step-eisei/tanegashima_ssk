9cb880f2b87f35e6490057648c62178943d6f5f4
shota182 <104834704+shota182@users.noreply.github.com>
2023-01-31 23:13:59 +0900

    Update class_distance.py

--- main/class_distance.py
+++ main/class_distance.py
@@ -6,45 +6,49 @@ import numpy as np
 
 class Distance:
     
-def __init__(self,TRIG=17,ECHO=27):
-    self.TRIG = TRIG
-    self.ECHO = ECHO
-    pass
+    def __init__(self,TRIG=17,ECHO=27):
+        self.TRIG = TRIG
+        self.ECHO = ECHO
+        pass
 
-def reading(self,sensor = 0):
+    def reading(self):
 
-    distance_data = []
+        distance_data = []
 
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
 
-    if sensor == 0:
         #データ取得
         cycle=1
         while(cycle<6):
-                GPIO.setup(self.TRIG,GPIO.OUT)
-                GPIO.setup(self.ECHO,GPIO.IN)
-                GPIO.output(self.TRIG, GPIO.LOW)
-                time.sleep(0.3)
-                GPIO.output(self.TRIG, True)
-                time.sleep(0.00001)
-                GPIO.output(self.TRIG, False)
-                error=0
-                debugtime = time.time()
-                while GPIO.input(self.ECHO) == 0:
-                    signaloff = time.time()
-                    if(signaloff-debugtime>1):
-                        GPIO.output(self.TRIG, False)
-                        print("overflow")
-                        error=1
-                        break
-                if(error==0):
-                    while GPIO.input(self.ECHO) == 1:
-                        signalon = time.time()
-                    timepassed = signalon - signaloff
-                    distance = timepassed * 17000
-                    distance_data.append(distance)
-                    cycle+=1
+            GPIO.setup(self.TRIG,GPIO.OUT)
+            GPIO.setup(self.ECHO,GPIO.IN)
+            GPIO.output(self.TRIG, GPIO.LOW)
+            time.sleep(0.3)
+            GPIO.output(self.TRIG, True)
+            time.sleep(0.00001)
+            GPIO.output(self.TRIG, False)
+            error=0
+            error_count = 0
+            debugtime = time.time()
+            while GPIO.input(self.ECHO) == 0:
+                signaloff = time.time()
+                if(signaloff-debugtime>1):
+                    GPIO.output(self.TRIG, False)
+                    print("overflow")
+                    error=1
+                    error_count+=1
+                    break
+            if(error==0):
+                while GPIO.input(self.ECHO) == 1:
+                    signalon = time.time()
+                timepassed = signalon - signaloff
+                distance = timepassed * 17000
+                distance_data.append(distance)
+                cycle+=1
+            if(error_count == 5):
+                self.distance = 10000
+                return -1
 
 
         #外れ値を除外   
@@ -56,12 +60,9 @@ def reading(self,sensor = 0):
                 num = i
                 max = (ave-distance_data[i])**2
         distance_data = np.delete(distance_data, num)
-#                 print(distance_data)
+        # print(distance_data)
         self.distance = np.mean(distance_data)
         GPIO.cleanup()
-
-    else:
-        print("Incorrect usonic() function varible.")
 
 
 def main():
@@ -73,9 +74,8 @@ def main():
             time.sleep(1)
         
     except KeyboardInterrupt:
-            GPIO.output(self.TRIG, False)
+            GPIO.output(distance.TRIG, False)
             GPIO.cleanup()
-    pass
 
 if __name__ == "__main__":
     main()
