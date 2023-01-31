a21e01ac3901091ab521251bffa1dbf83601dd77
kaise-m <90245285+kaise-m@users.noreply.github.com>
2023-01-31 22:56:26 +0900

    Update phase_land.py

--- main/phase_land.py
+++ main/phase_land.py
@@ -3,7 +3,7 @@ import time
 
 class Land:
     
-    def __init__(self,get_pressure,sky=0.1, land=0.01): #地上気圧測定
+    def __init__(self,get_pressure,sky=0.1, land=0.2): #地上気圧測定
         self.sky=sky
         self.land=land
         self.i=0
@@ -13,15 +13,16 @@ class Land:
             sum_pressure=0.0
             sum_pressure = sum_pressure + self.get_pressure.pressure
             self.i=self.i+1
-        self.land=sum_pressure/10
-        print(self.land)
+            time.sleep(0.1)
+        self.land_pressure=sum_pressure/10
+        print(self.land_pressure)
         self.i=0
 
     def sky_pressure(self):
         self.i=0
         while self.i<=10:
             self.get_pressure.read() #毎回pressure更新
-            if self.land - self.get_pressure.pressure > self.sky: #閾値暫定
+            if self.land_pressure-self.get_pressure.pressure > self.sky: #閾値暫定
                 print(self.i)
                 self.i=self.i+1
             else: self.i=0 #やり直し
@@ -33,7 +34,7 @@ class Land:
         self.i=0
         while self.i<=10:
             self.get_pressure.read() #毎回pressure更新
-            if self.get_pressure.pressure - self.land < self.land: #閾値暫定
+            if self.land_pressure-self.get_pressure.pressure < self.land: #閾値暫定
                 print(self.i)
                 self.i=self.i+1
             else: self.i=0 #やり直し
