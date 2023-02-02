948b96ddaf4ad6a609ba7d6e36bf12c0284dbb09
unknown <taroarima2014@gmail.com>
2023-02-02 20:21:55 +0900

    modified:   main/class_gps.py

--- main/class_gps.py
+++ main/class_gps.py
@@ -15,12 +15,14 @@ class Gps:
         self.ser.readline() # 最初の1行は中途半端なデーターが読めることがあるので、捨てる
 
     def getgps(self):
-        sentences = self.ser.read_all().decode("utf-8")
-        for s in sentences:
-            if s[0] != '$':
-                continue
+        s = self.ser.readline().decode("utf-8")
+        #for s in sentences:
+        if s[0] != '$':
+            pass
+        else: 
             for x in s:
                 self.gps.update(x)
+                print("updated")
         self.latitude = self.gps.latitude[0]
         self.longitude = self.gps.longitude[0]
     
