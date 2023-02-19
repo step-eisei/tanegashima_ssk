225e413d4fbf2517617acebe6559c9d5baebd9cf
STEPeisei <step.eisei@gmail.com>
2023-02-19 13:27:18 +0900

    by ssk

--- main/calibration_geomag.py
+++ main/calibration_geomag.py
@@ -1,5 +1,7 @@
 from class_motor import Motor
 import time
+import datetime
+import csv
 
 def percentpick(listdata, p):
         n = int(len(listdata) *p/100)
@@ -12,7 +14,7 @@ motor = Motor()
 
 duty = 8
 p = 5
-time_all = 120
+time_all = 60
 duration = 0.5
 
 t = 0
@@ -35,6 +37,15 @@ motor.end()
 magxs = [mag_list[i][0] for i in range(len(mag_list))]
 magys = [mag_list[i][1] for i in range(len(mag_list))]
 magzs = [mag_list[i][2] for i in range(len(mag_list))]
+
+# csv save
+DIFF_JST_FROM_UTC = 9
+jp_time = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
+recordname = 'mag/mag_' + str(jp_time).replace(' ', '_').replace(':', '-').replace('.', '_') + '.csv'
+with open(recordname, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["magx", "magy", "magz"])
+    writer.writerows(mag_list)
 
 # 最大値，最小値の算出
 Xmax, Xmin = percentpick(magxs, p)
