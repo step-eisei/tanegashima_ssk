06fd5425f45b1ff5b1860204ff0dc1daf09cf92b
unknown <taroarima2014@gmail.com>
2023-02-22 15:49:37 +0900

    modified

--- main/phase_camera.py
+++ main/phase_camera.py
@@ -9,7 +9,7 @@ import time
 import math
 
 class Phase_camera:
-    def __init__(self, yolo=class_yolo.CornDetect(), motor=class_motor.Motor(geomag = class_geomag.GeoMagnetic(rads=[19.545454545454543, 14.500000000000004, 26.07142857142857], aves=[-77.0, 28.136363636363637, -122.8061224489796])), distance=class_distance.Distance()): #, subthread=subthread.Subthread()):
+    def __init__(self, yolo=class_yolo.CornDetect(), motor=class_motor.Motor(geomag = class_geomag.GeoMagnetic(rads=[26.363636363636367, 17.409090909090907, 32.80612244897959], aves=[48.18181818181818, -109.5, 117.90816326530613])), distance=class_distance.Distance()): #, subthread=subthread.Subthread()):
         self.yolo = yolo
         self.motor = motor
         self.distance = distance
