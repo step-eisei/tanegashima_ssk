3613f0e8dc7684dd41afecc8b32e44919b252210
unknown <taroarima2014@gmail.com>
2023-02-23 21:59:51 +0900

    modified

--- main/phase_land.py
+++ main/phase_land.py
@@ -4,47 +4,53 @@ import time
 
 class Land:
     
-    def __init__(self,get_pressure=class_pressure.Pressure(),subthread=subthread.Subthread(),sky=0.1, land=0.2): #地上気圧測定
-        self.sky=sky
-        self.land=land
-        self.i=0
-        self.get_pressure=get_pressure
-        self.subthread=subthread
+    def __init__(self,get_pressure=None,subth=None,sky=0.1, land=0.2): #地上気圧測定
+        if get_pressure == None: self.get_pressure = class_pressure.Pressure()
+        else:                    self.get_pressure = get_pressure
+
+        #if subth == None:        self.subthread = subthread.Subthread()
+        #else:                    self.subthread = subth
+
+        self.sky=sky   #上空まで上がったか判定するときの大気圧の変化の閾値 要調整
+        self.land=land #地上まで降りたか、判定するときの大気圧の変化の閾値 要調整
+
+        #最初に地上の大気圧を測る
+        i=0
         sum_pressure=0.0
-        while self.i<10:
+        while i<10:
             self.get_pressure.read()
             sum_pressure = sum_pressure + self.get_pressure.pressure
-            self.i=self.i+1
+            i=i+1
             time.sleep(0.5)
         self.start_pressure=sum_pressure/10
         print(self.start_pressure)
-        self.i=0
-
+    
+    #上空に上がったか判定
     def sky_pressure(self):
-        self.i=0
-        while self.i<=10:
+        i=0
+        while i<=10:
             self.get_pressure.read() #毎回pressure更新
             print(self.get_pressure.pressure)
             if self.start_pressure-self.get_pressure.pressure > self.sky: #閾値暫定
-                print(self.i)
-                self.i=self.i+1
+                print(i)
+                i = i + 1
             else: 
-                self.i=0 #やり直し
+                i=0 #やり直し
                 print("yet")
             time.sleep(0.5)
         print("sky")
-        self.i=0
-
+    
+    #地上まで降りたか判定
     def land_pressure(self):
-        self.i=0
-        while self.i<=10:
+        i=0
+        while i<=10:
             self.get_pressure.read() #毎回pressure更新
             print(self.get_pressure.pressure)
             if self.start_pressure-self.get_pressure.pressure < self.land: #閾値暫定
-                print(self.i)
-                self.i=self.i+1
+                print(i)
+                i=i+1
             else:
-                self.i=0 #やり直し
+                i=0 #やり直し
                 print("yet")
             time.sleep(0.5)
         print("land")
@@ -64,4 +70,3 @@ def main(sky=0.1, land=0.01): #上空判定，地上判定の閾値
 
 if __name__ == "__main__":
     main()
-    pass
