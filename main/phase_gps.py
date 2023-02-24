9d93be4b4acb4c54ee58a4e0ae1f51463b7dc0a4
unknown <taroarima2014@gmail.com>
2023-02-24 15:16:58 +0900

    modified

--- main/phase_gps.py
+++ main/phase_gps.py
@@ -42,7 +42,7 @@ class Gps_phase():
         self.renew_data() #gpsと地磁気を取得して更新
 
     def run(self, duty_max=25):
-        self.subthread.phase = 2
+        #elf.subthread.phase = 2
         first = True
         duty_R = duty_L = duty_max
         stack = 0
