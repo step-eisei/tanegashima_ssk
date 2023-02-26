5509f6028022dbf6dd85ea8b7dc895f37e425503
unknown <taroarima2014@gmail.com>
2023-02-26 11:27:42 +0900

    modified test_camera

--- moduletest/camera/test_camera.py
+++ moduletest/camera/test_camera.py
@@ -47,8 +47,9 @@ def automatic_brightness_and_contrast(image, clip_hist_percent=25):
     return auto_result, alpha, beta
 	
 camera = cv2.VideoCapture(0, cv2.CAP_V4L2)
-camera.set(cv2.CAP_PROP_AUTO_WB, 1)
-camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+print(cv2.CAP_PROP_AUTO_EXPOSURE)
+#camera.set(cv2.CAP_PROP_AUTO_WB, 1)
+#camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
 success, image = camera.read()
 #image = cv2.convertScaleAbs(image, alpha=1.0, beta=-50)
 #image, _, _ = automatic_brightness_and_contrast(image)
@@ -76,7 +77,7 @@ success, image = camera.read()
 if not success:
       print("failed")
 else:
-      cv2.imwrite("test.jpg", image)
+      cv2.imwrite("test_after.jpg", image)
       print(image.shape)
 
 camera.release()
